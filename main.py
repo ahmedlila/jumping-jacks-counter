8860cd366052c5707a199b9d81d3239c907dadaf
Ahmed Lila <ahmedelila7@gmail.com>
2023-09-02 06:15:00 +0300

    V1.0 completed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import argparse
+
 
 # helper function
 def calculate_angle(a,b,c)->float:
@@ -22,96 +24,87 @@ def calculate_angle(a,b,c)->float:
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-# cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture("3 Count Jumping Jacks.mp4")
+def main(filepath):
+    # cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(filename=filepath)  # for webcam replace string with 0
 
+    # Initalizations
+    counter = 0 
+    stage = None
+    MAX_UP = 100
+    MAX_DOWN = 60 
 
-# Curl counter variables
-counter = 0 
-stage = None
-
-## Setup mediapipe instance
-with mp_pose.Pose(min_detection_confidence=0.2, min_tracking_confidence=0.2) as pose:
-    while cap.isOpened():
-        ret, frame = cap.read()
-        
-        # Recolor image to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-        
-        # Make detection
-        results = pose.process(image)
-
-        # Recolor back to BGR
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        
-        # Extract landmarks
-        try:
-            landmarks = results.pose_landmarks.landmark
+    # Setup mediapipe instance
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
             
-            # Get coordinates
-            right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-            right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
-            right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
-            right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
-            left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-            left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-            left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
-
-            # Calculate angle
-            right_angle = calculate_angle(right_hip, right_shoulder, right_elbow)
-            left_angle = calculate_angle(left_hip, left_shoulder, left_elbow)
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+        
+            # Make detection
+            results = pose.process(image)
+        
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
             
-            # Visualize angle
-            cv2.putText(image, str(right_angle), 
-                           tuple(np.multiply(right_shoulder, [640, 480]).astype(int)), 
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                )
-            cv2.putText(image, str(left_angle), 
-                           tuple(np.multiply(right_shoulder, [640, 480]).astype(int)), 
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-                                )
+            # Extract landmarks
+            try:
+                landmarks = results.pose_landmarks.landmark
+                
+                # Get coordinates
+                right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+                left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+                
+                # Calculate angle
+                right_angle = calculate_angle(right_hip, right_shoulder, right_elbow)
+                left_angle = calculate_angle(left_hip, left_shoulder, left_elbow)
+                
+                
+                # Curl counter logic
+                if right_angle > MAX_UP and left_angle > MAX_UP:
+                    stage = "up"
+                if right_angle < MAX_DOWN and left_angle < MAX_DOWN and stage =='up':
+                    stage="down"
+                    counter +=1
+            except:
+                pass
             
-            # Curl counter logic
-            if right_angle > 120 and left_angle > 120:
-                stage = "up"
-            if right_angle < 50 and left_angle < 50 and stage =='up':
-                stage="down"
-                counter +=1
-        except:
-            pass
-        
-        # Render curl counter
-        # Setup status box
-        cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
-        
-        # Rep data
-        cv2.putText(image, 'REPS', (15,12), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(image, str(counter), 
-                    (10,60), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
-        
-        # Stage data
-        cv2.putText(image, 'STAGE', (65,12), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(image, stage, 
-                    (60,60), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
-        
-        
-        # Render detections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
-                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
-                                 )               
-        
-        cv2.imshow('Mediapipe Feed', image)
+            # Render curl counter
+            # Setup status box
+            cv2.rectangle(image, (0,0), (140,70), (245,117,16), -1)
+            
+            # Rep data
+            cv2.putText(image, 'REPS', (15,12), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(counter), 
+                        (10,50), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+            
+            # Stage data
+            cv2.putText(image, 'STAGE', (65,12), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, stage, 
+                        (60,50), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+            cv2.imshow('Mediapipe Feed', image)
 
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+        
+        cap.release()
+        cv2.destroyAllWindows()
 
-    cap.release()
-    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filepath", type=str,
+        help="Location of the source video file or Webcam")
+    args = parser.parse_args()
+    main(filepath=args.filepath)
